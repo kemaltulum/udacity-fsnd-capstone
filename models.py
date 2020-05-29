034acddeafc25a92f395cb6915a365c3e4892bd5
kemaltulum <tulum.ke@gmail.com>
2020-05-30 01:46:45 +0300

    A bug fix on migrations
    db.create_all is removed, it was creating all the necessary relations on the db, which makes migration commands not to realize any change to migrate.

--- models.py
+++ models.py
@@ -21,7 +21,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
     migrate = Migrate(app, db)
 
 '''
