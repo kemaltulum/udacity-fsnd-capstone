d40f947d8bb9399ed4e1ea20c0a1af5dd343afa4
kemaltulum <tulum.ke@gmail.com>
2020-05-30 01:16:53 +0300

    Bug fix on manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app
+from flaskr import create_app
 from models import db
 
+app = create_app()
 migrate = Migrate(app, db)
 manager = Manager(app)
 
