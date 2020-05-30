c89f81107673fda76bfcad88f8e83012d0192b61
kemaltulum <tulum.ke@gmail.com>
2020-05-30 11:43:30 +0300

    Update models.py according to pep8 guideline

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import os
-from sqlalchemy import ForeignKey, Column, String, Integer, DateTime, create_engine
+from sqlalchemy import ForeignKey, Column, String, Integer, \
+                    DateTime, create_engine
 from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -14,84 +15,93 @@ db = SQLAlchemy()
 
 '''
 setup_db(app)
-    binds a flask application and a SQLAlchemy service
+        binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
+
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     migrate = Migrate(app, db)
 
+
 '''
 Movie
 '''
-class Movie(db.Model):  
-  __tablename__ = 'movies'
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  release_date = Column(DateTime)
-  actors = relationship('Actor', backref="movie", lazy=True)
 
-  def __init__(self, title, release_date):
-    self.title = title
-    self.release_date = release_date
+class Movie(db.Model):
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    __tablename__ = 'movies'
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+    actors = relationship('Actor', backref="movie", lazy=True)
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date,
-      'actors': list(map(lambda actor: actor.format(), self.actors))
-    }
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+            'actors': list(map(lambda actor: actor.format(), self.actors))
+        }
 
 '''
 Actor
 '''
-class Actor(db.Model):  
-  __tablename__ = 'actors'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  age = Column(Integer)
-  gender = Column(String)
-  movie_id = Column(Integer, ForeignKey('movies.id'), nullable=True)
 
-  def __init__(self, name, age, gender, movie_id):
-    self.name = name
-    self.age = age
-    self.gender = gender
-    self.movie_id = movie_id
+class Actor(db.Model):
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    __tablename__ = 'actors'
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+    movie_id = Column(Integer, ForeignKey('movies.id'), nullable=True)
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender,
-      "movie_id": self.movie_id
-    }
+    def __init__(self, name, age, gender, movie_id):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.movie_id = movie_id
 
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+            "movie_id": self.movie_id
+        }
